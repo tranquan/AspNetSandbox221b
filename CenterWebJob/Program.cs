769a4f504068026c0893fe0d37409bad69cc5624
Quan Tran <tranquan221b@gmail.com>
2019-08-11 23:51:57 +0200

    Initial commit

---
+++ CenterWebJob/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace CenterWebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            // Initialize();
+            Console.WriteLine("CenterWebJob started");
+
+            //var config = new JobHostConfiguration();
+
+            //if (config.IsDevelopment)
+            //{
+            //    config.UseDevelopmentSettings();
+            //}
+
+            //config.DashboardConnectionString = "UseDevelopmentStorage=true;";
+            //config.StorageConnectionString = "UseDevelopmentStorage=true;";
+
+            // var host = new JobHost(config);
+            //// The following code ensures that the WebJob will be running continuously
+            // host.RunAndBlock();
+
+            var host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        static void Initialize()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            var prefix = "environment-";
+            var envKeys = appSettings.AllKeys.Where(s => s.StartsWith(prefix)).ToList();
+            envKeys.ForEach(key => {
+                var realKey = key.Substring(prefix.Length);
+                var value = appSettings[key];
+               //  value = ReadFromKeyVaultIfRequired(realKey, value);
+                Environment.SetEnvironmentVariable(realKey, value);
+            });
+        }
+    }
+}
---
+++ CenterWebJobTrigger/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterWebJobTrigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunOnBackground().Wait();
+        }
+
+        static async Task RunOnBackground()
+        {
+            var conn1 = ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+            var conn2 = CloudConfigurationManager.GetSetting("StorageConnectionString");
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(conn1);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            CloudQueue queue = queueClient.GetQueueReference("sandbox221b");
+            _ = await queue.CreateIfNotExistsAsync();
+
+            CloudQueueMessage message = new CloudQueueMessage("Hey! a message is commming");
+            await queue.AddMessageAsync(message);
+
+            CloudQueueMessage peekedMessage = queue.PeekMessage();
+
+            Console.WriteLine(peekedMessage.AsString);
+        }
+    }
+}
